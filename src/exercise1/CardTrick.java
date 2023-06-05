186680f60094c50e05936b5e7010c4840120b52e
liangwan2023 <133809170+liangwan2023@users.noreply.github.com>
2023-06-05 10:59:35 -0400

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -73,16 +73,16 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Wanqin Liang, but you can call me Wanqin");
         System.out.println();
         
-        System.out.println("My career ambitions:");
+        System.out.println("My career ambitions: teacher");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Hiking");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
